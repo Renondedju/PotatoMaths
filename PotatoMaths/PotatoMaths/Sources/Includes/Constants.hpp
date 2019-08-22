5034d81ddf03ec8ac4419c6d930814be48c9387b
Renondedju <lefantomedelaredstone@gmail.com>
2019-08-22 17:07:01 +0200

    [Added] Constants

---
+++ PotatoMaths/PotatoMaths/Sources/Includes/Constants.hpp
@@ -0,0 +1,42 @@
+/*
+ * MIT License
+ * 
+ * Copyright (c) 2019 Renondedju, 6xray
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#pragma once
+
+#define POTATO_E			2.71828182845904523536   // e
+#define POTATO_LOG2E		1.44269504088896340736   // log2(e)
+#define POTATO_LOG10E		0.434294481903251827651  // log10(e)
+#define POTATO_LN2			0.693147180559945309417  // ln(2)
+#define POTATO_LN10			2.30258509299404568402   // ln(10)
+#define POTATO_PI			3.14159265358979323846   // pi
+#define POTATO_PI_2			1.57079632679489661923   // pi/2
+#define POTATO_PI_4			0.785398163397448309616  // pi/4
+#define POTATO_1_PI			0.318309886183790671538  // 1/pi
+#define POTATO_2_PI			0.636619772367581343076  // 2/pi
+#define POTATO_2_SQRTPI		1.12837916709551257390   // 2/sqrt(pi)
+#define POTATO_SQRT2		1.41421356237309504880   // sqrt(2)
+#define POTATO_SQRT1_2		0.707106781186547524401  // 1/sqrt(2)
+
+#define POTATO_FLT_EPSILON	1.192092896e-07F        // smallest such that 1.0+FLT_EPSILON != 1.0
+#define POTATO_DBL_EPSILON	2.2204460492503131e-016 // smallest such that 1.0+DBL_EPSILON != 1.0
