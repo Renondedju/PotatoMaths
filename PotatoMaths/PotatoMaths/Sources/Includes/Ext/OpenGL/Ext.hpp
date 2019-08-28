c78ad57d593d475c585f5ec4652e28f5c326f404
Renondedju <lefantomedelaredstone@gmail.com>
2019-08-28 16:34:41 +0200

    [Added] Extensions

---
+++ PotatoMaths/PotatoMaths/Sources/Includes/Ext/OpenGL/Ext.hpp
@@ -0,0 +1,39 @@
+/*
+ * MIT License
+ * 
+ * Copyright (c) 2019 Renondedju, 6xray
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#pragma once
+
+/**
+ * --- Potato maths OpenGL extension header ---
+ *
+ * This extension implements every OpenGL specific operations and maths
+ */
+
+#define POTATO_GL_EXT_NAMESPACE PotatoGlExt
+#define POTATO_BEGIN_GL_EXT namespace POTATO_GL_EXT_NAMESPACE {
+#define POTATO_END_GL_EXT }
+
+#define POTATO_USING_GL_EXT using namespace POTATO_GL_EXT_NAMESPACE;
+
+namespace POTATO_GL_EXT_NAMESPACE {}
